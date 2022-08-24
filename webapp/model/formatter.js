d855d2142ba6482ee10406cf094bfb62c449cfe8
Sagar <SAGAR.M>
2022-08-24 17:23:11 +0530

    new changes

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -9,21 +9,29 @@ sap.ui.define([], function () {
          * @param {string} sValue the number string to be rounded
          * @returns {string} sValue with 2 digits rounded
          */
-        numberUnit : function (sValue) {
+        numberUnit: function (sValue) {
             if (!sValue) {
                 return "";
             }
             return parseFloat(sValue).toFixed(2);
         },
-        iconFilePath : function(v) {
+        iconFilePath: function (v) {
             var path = "";
             // if (v = "X"){
             // 	path = jQuery.sap.getModulePath("com.sap.byjus.byjusdashboard", "/icon/TrafficLightG.png");
             // } else{
             // 	 path = jQuery.sap.getModulePath("com.sap.byjus.byjusdashboard", "/icon/TrafficLightG.png");
             // }
-        return path;
-     },
+            return path;
+        },
+        formatNumberIntoCurrency: function (sValue) {
+            var formatter = new Intl.NumberFormat('en-IN', {
+                style: 'currency',
+                currency: 'INR',
+            });
+
+           return formatter.format(parseInt(sValue));
+        }
 
     };
 
